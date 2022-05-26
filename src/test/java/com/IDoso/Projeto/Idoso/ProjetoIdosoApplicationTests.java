ce5718b32e684bd331d83b922ea16325c0d46362
gabrie2121 <gabriel.miranda041200@gmail.com>
2022-05-26 20:26:32 -0300

    projeto inicial

---
+++ src/test/java/com/IDoso/Projeto/Idoso/ProjetoIdosoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IDoso.Projeto.Idoso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoIdosoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
