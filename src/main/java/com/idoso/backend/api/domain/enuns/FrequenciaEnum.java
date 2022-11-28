d7714673fb9c6a3453b5947a9383fd39afbb1555
bfaisca <bmlfaisca@gmail.com>
2022-11-28 20:36:37 -0300

    COrreção enum

--- src/main/java/com/idoso/backend/api/domain/enuns/FrequenciaEnum.java
+++ src/main/java/com/idoso/backend/api/domain/enuns/FrequenciaEnum.java
@@ -1,5 +1,5 @@
 package com.idoso.backend.api.domain.enuns;
 
 public enum FrequenciaEnum {
-    UMDIA, DOISDIAS, DIASIMNAO, SEGUNDASEXTA, FINALDEMANA, TODOSDIAS;
+    UMDIA, DOISDIAS, DIASIMNAO, SEGUNDASEXTA, FINALSEMANA, TODOSDIAS;
 }
